dc093577df149b3bc5c4af746d4b67a54e829130
Alexis Hassler <alexis.hassler@sewatech.fr>
2014-08-09 16:05:05 +0200

    #35 Build fails with empty repository - 6.2.0

--- build-eap.sh
+++ build-eap.sh
@@ -7,10 +7,9 @@ set_version $1
 make_directory -f work
 make_directory download
 make_directory dist
-download_and_unzip ftp://ftp.redhat.com/redhat/jbeap/$EAP_VERSION/en/source/$SRC_FILE
+download_and_unzip http://ftp.redhat.com/redhat/jbeap/$EAP_VERSION/en/source/$SRC_FILE
 download_and_unzip http://maven.repository.redhat.com/techpreview/eap6/$EAP_VERSION/$MVN_FILE
 patch_files
 set_repository_url
 maven_build
 save_result
-
