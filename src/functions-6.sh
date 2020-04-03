6cbff527f0a3d9f30d668f52f4273b2b97287f79
Alexis Hassler <alexis.hassler@sewatech.fr>
2020-04-03 10:08:22 +0200

    Fix 6.3.3 #139

--- src/functions-6.sh
+++ src/functions-6.sh
@@ -38,7 +38,6 @@ function patch_files {
         cd ../..
     fi
     cp src/settings.xml work/jboss-eap-$EAP_SHORT_VERSION-src/tools/maven/conf/settings.xml
-#    cp src/settings-ea.xml work/jboss-eap-$EAP_SHORT_VERSION-src/tools/maven/conf/settings.xml
     cp src/build.conf work/jboss-eap-$EAP_SHORT_VERSION-src/
 }
 
