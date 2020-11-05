71d83931cd9a466d45f3f51f6f7783ff28305395
Oskar Stephens <oskar.stephens@sheerid.com>
2020-11-05 12:40:17 -0800

    Fix redhat source location and maven settings
    The url for jboss source changed recently-ish. Updated to the new
    structure.
    
    Updated the maven settings file to use https, which is now required by
    maven central.

--- build-eap.sh
+++ build-eap.sh
@@ -7,7 +7,7 @@ set_version $1
 make_directory -f work
 make_directory download
 make_directory dist
-download_and_unzip http://ftp.redhat.com/redhat/jbeap/$EAP_VERSION/en/source/$SRC_FILE
+download_and_unzip http://ftp.redhat.com/redhat/jboss/eap/$EAP_VERSION/en/source/$SRC_FILE
 patch_files
 build_core
 maven_build
